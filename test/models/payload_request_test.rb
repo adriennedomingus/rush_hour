6a8e8dee0831ae5e1f91ebfec724742737185d6b
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-09 09:21:58 -0700

    migration setup

---
+++ test/models/payload_request_test.rb
@@ -0,0 +1,26 @@
+require_relative '../test_helper'
+
+class PayloadRequestTest < MiniTest::Test
+  include TestHelpers
+
+  def test_has_all_attributes_with_valid_input
+    payload = {
+      "url":"http://jumpstartlab.com/blog",
+      "requestedAt":"2013-02-16 21:38:28 -0700",
+      "respondedIn":37,
+      "referredBy":"http://jumpstartlab.com",
+      "requestType":"GET",
+      "parameters":[],
+      "eventName": "socialLogin",
+      "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth":"1920",
+      "resolutionHeight":"1280",
+      "ip":"63.29.38.211"
+    }
+
+    #Something to connect to test database?
+
+    assert_equal 1, PayloadRequest.all.count
+    assert_equal "http://jumpstartlab.com/blog", PayloadRequest.first.url
+  end
+end
