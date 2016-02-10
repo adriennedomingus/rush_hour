b2f460f0945f4029354b89617d0208258759839c
Adam Hundley <adamhundley@gmail.com>
2016-02-09 19:55:06 -0700

    minor refactor

--- app/models/payload_parser.rb
+++ app/models/payload_parser.rb
@@ -1,6 +1,5 @@
 
 class PayloadParser
-
   def initialize(payload)
     payload_into_hash(payload)
   end
@@ -10,12 +9,11 @@ class PayloadParser
       :url          => payload[:url],
       :requested_at => payload[:requestedAt],
       :responded_in => payload[:respondedIn],
-      :referral     => Referral.create(path: payload[:referredBy]),
-      :verb         => RequestType.create(verb: payload[:requestType]),
-      :event_name   => EventName.create(event: payload[:eventName]),
+      :referral     => Referral.find_or_create_by(path: payload[:referredBy]),
+      :verb         => RequestType.find_or_create_by(verb: payload[:requestType]),
+      :event_name   => EventName.find_or_create_by(event: payload[:eventName]),
       :environment  => UserAgent.parse(payload[:userAgent]),
-      :resolution   => Resolution.create(width: payload[:resolutionWidth], height: payload[:resolutionHeight]),
+      :resolution   => Resolution.find_or_create_by(width: payload[:resolutionWidth], height: payload[:resolutionHeight]),
       :ip           => payload[:ip]}
   end
-
 end
