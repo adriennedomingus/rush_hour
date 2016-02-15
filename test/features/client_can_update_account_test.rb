ac6fcdb4f93a1848d70b11b10d36bdcb044359f7
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-15 09:52:05 -0700

    test for update functionality

---
+++ test/features/client_can_update_account_test.rb
@@ -0,0 +1,16 @@
+require_relative '../test_helper'
+
+class ClientCanUpdateAccountTest < FeatureTest
+  include TestHelpers
+
+  def test_client_update_account
+    page.driver.browser.post('/sources?identifier=jumpstartlab&rooturl=http://jumpstartlab.com')
+    assert_equal 'jumpstartlab', Client.all.first.identifier
+
+    visit '/jumpstartlab/update'
+    fill_in 'client[identifier]', with: 'changedmyname'
+    click_button 'Submit'
+    assert_equal 'changedmyname', Client.all.first.identifier
+  end
+
+end
