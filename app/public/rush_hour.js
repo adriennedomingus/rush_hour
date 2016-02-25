287f5c42d74224162c5639481a1f5421b0bdf7f5
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-02-25 10:03:02 -0700

    trying js

--- app/public/rush_hour.js
+++ app/public/rush_hour.js
@@ -9,7 +9,7 @@ RushHour.track = function(clientName, eventName) {
     //"responseTime":unc,
     "requestType": "GET",
     "parameters": window.location.search.replace("?","").split("&"),
-    "eventName": eventName,
+    "eventName": eventName ,
     "userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
     "resolutionWidth":"1920",
     "resolutionHeight":"1280",
